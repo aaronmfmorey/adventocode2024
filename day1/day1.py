f52b6710be2e89a777e6860d79389d230f0e7455
Aaron Morey <aaronmorey@gmail.com>
2024-12-02 23:48:10 -0600

    Day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -16,3 +16,20 @@ for i in range(0, len(a)):
     total += abs(a[i] - b[i])
 
 print(total)
+
+bCounts = dict()
+for bi in b:
+    if bi in bCounts:
+        bCounts[bi] = bCounts[bi] + 1
+    else:
+        bCounts[bi] = 1
+
+similarity = 0
+for ai in a:
+    aiCount = bCounts.get(ai)
+    if aiCount == None:
+        aiCount = 0
+    sim = ai * aiCount
+    similarity += sim
+
+print(similarity)
